e0a497aa6f2878e066af1d873b40fb1e261c12b1
Sodiq Fagbola <damilolasodiq@gmail.com>
2018-08-11 16:29:43 +0100

    make default value static

--- src/main/java/com/mightwork/searchapi/SearchConfigurer.java
+++ src/main/java/com/mightwork/searchapi/SearchConfigurer.java
@@ -1,25 +1,24 @@
 package com.mightwork.searchapi;
 
 import com.mightwork.searchapi.data.SearchKey;
+
 import java.lang.reflect.ParameterizedType;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-public abstract class SearchConfigurer<T>
-{
+public abstract class SearchConfigurer<T> {
+    private static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
 
     public abstract List<SearchKey> getSearchKeys();
 
 
-    public SimpleDateFormat getDateKeyFormat()
-    {
-        return new SimpleDateFormat("yyyy-MM-dd");
+    public SimpleDateFormat getDateKeyFormat() {
+        return new SimpleDateFormat(DEFAULT_DATE_FORMAT);
     }
 
 
-    public final Class<T> getType()
-    {
+    public final Class<T> getType() {
         return (Class<T>) ((ParameterizedType) this.getClass()
-            .getGenericSuperclass()).getActualTypeArguments()[0];
+                .getGenericSuperclass()).getActualTypeArguments()[0];
     }
 }
