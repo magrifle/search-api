ce789fff0c4027405681b12f8fdf25accf8df165
Sodiq Fagbola <damilolasodiq@gmail.com>
2018-08-11 15:14:16 +0100

    add constructor in searchkey

--- src/main/java/com/mightwork/searchapi/data/SearchKey.java
+++ src/main/java/com/mightwork/searchapi/data/SearchKey.java
@@ -2,75 +2,70 @@ package com.mightwork.searchapi.data;
 
 import com.mightwork.searchapi.SearchOperation;
 
-public class SearchKey
-{
+public class SearchKey {
     private String name;
 
     private SearchOperation[] deniedOperations;
+
     private String fieldName;
 
     private boolean required;
 
 
-    public SearchKey(String name, String fieldName, boolean required)
-    {
+    public SearchKey(String name, String fieldName, boolean required) {
         this.name = name;
         this.fieldName = fieldName;
         this.required = required;
     }
 
 
-    public SearchKey(String name, String fieldName)
-    {
+    public SearchKey(String name, String fieldName) {
         this.name = name;
         this.fieldName = fieldName;
     }
 
+    public SearchKey(String fieldName) {
+        this.name = fieldName;
+        this.fieldName = fieldName;
+    }
 
-    public String getName()
-    {
+
+    public String getName() {
         return name;
     }
 
 
-    public void setName(String name)
-    {
+    public void setName(String name) {
         this.name = name;
     }
 
 
-    public SearchOperation[] getDeniedOperations()
-    {
+    public SearchOperation[] getDeniedOperations() {
         return deniedOperations;
     }
 
 
-    public void setDeniedOperations(SearchOperation[] deniedOperations)
-    {
+    public void setDeniedOperations(SearchOperation[] deniedOperations) {
         this.deniedOperations = deniedOperations;
     }
 
 
-    public String getFieldName()
-    {
+    public String getFieldName() {
         return fieldName;
     }
 
 
-    public void setFieldName(String fieldName)
-    {
+    public void setFieldName(String fieldName) {
         this.fieldName = fieldName;
     }
 
 
-    public boolean isRequired()
-    {
+    public boolean isRequired() {
         return required;
     }
 
 
-    public void setRequired(boolean required)
-    {
+    public void setRequired(boolean required) {
         this.required = required;
     }
 }
