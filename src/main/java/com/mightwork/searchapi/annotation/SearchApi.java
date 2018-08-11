1949cefb041e59e734ee8b4dc953b49b120ba4e3
Sodiq Fagbola <damilolasodiq@gmail.com>
2018-08-11 14:40:44 +0100

    remove alias for entity in search api annotation

--- src/main/java/com/mightwork/searchapi/annotation/SearchApi.java
+++ src/main/java/com/mightwork/searchapi/annotation/SearchApi.java
@@ -5,19 +5,18 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 import org.springframework.core.annotation.AliasFor;
 
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface SearchApi
-{
+public @interface SearchApi {
 
     String queryString() default "q";
 
     char keySeparator() default ',';
 
-    @AliasFor("value")
     Class entity();
 
     boolean failOnMissingQueryString() default false;
