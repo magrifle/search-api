8d6779212c6bcaaf10101f23fa42777259e00753
sodiq <damilolasodiq@gmail.com>
2024-09-27 09:17:10 -0400

    WIP fix null filter values

--- src/main/java/com/github/magrifle/data/searchapi/specification/EntitySpecification.java
+++ src/main/java/com/github/magrifle/data/searchapi/specification/EntitySpecification.java
@@ -2,15 +2,12 @@ package com.github.magrifle.data.searchapi.specification;
 
 import com.github.magrifle.data.searchapi.SearchOperation;
 import com.github.magrifle.data.searchapi.data.SearchCriteria;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.*;
 import java.util.Arrays;
 import java.util.Date;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import org.springframework.data.jpa.domain.Specification;
+import java.util.Objects;
 
 public class EntitySpecification<T> implements Specification<T> {
 
@@ -26,22 +23,17 @@ public class EntitySpecification<T> implements Specification<T> {
 
     @Override
     public Predicate toPredicate(
-        Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder)
-    {
-        if (criteria == null)
-        {
+            Root<T> root, CriteriaQuery<?> query, CriteriaBuilder builder) {
+        if (criteria == null) {
             return null;
         }
 
         Path path;
         String[] joins = criteria.getKey().split("\\.");
-        if (joins.length > 1)
-        {
+        if (joins.length > 1) {
             Join join = root.join(joins[0]);
             path = criteria.getType() != null ? builder.treat(join, criteria.getType()).get(joins[1]) : join.get(joins[1]);
-        }
-        else
-        {
+        } else {
             path = root.get(criteria.getKey());
             query.distinct(true);
         }
@@ -50,14 +42,17 @@ public class EntitySpecification<T> implements Specification<T> {
     }
 
 
-    private Predicate getCriteria(CriteriaBuilder builder, Path path)
-    {
-        switch (criteria.getOperation())
-        {
+    private Predicate getCriteria(CriteriaBuilder builder, Path path) {
+        switch (criteria.getOperation()) {
             case EQUALITY:
             case NEGATION:
-                Predicate predicate = isCaseInsensitive(criteria, path) ? builder.equal(builder.lower(path), criteria.getValue().toString().toLowerCase()) :
-                        builder.equal(path, criteria.getValue());
+                Predicate predicate;
+                if (Objects.isNull(criteria.getValue())) {
+                    predicate = builder.isNull(path);
+                } else {
+                    predicate = isCaseInsensitive(criteria, path) ? builder.equal(builder.lower(path), criteria.getValue().toString().toLowerCase()) :
+                            builder.equal(path, criteria.getValue());
+                }
                 return criteria.getOperation() == SearchOperation.NEGATION ? predicate.not() : predicate;
             case GREATER_THAN:
                 return path.getJavaType() == Date.class ? builder.greaterThan(path, ((Date) criteria.getValue())) : builder.greaterThan(path, criteria.getValue().toString());
@@ -78,22 +73,22 @@ public class EntitySpecification<T> implements Specification<T> {
             case IN:
                 CriteriaBuilder.In inClause = isCaseInsensitive(criteria, path) ? builder.in(builder.lower(path)) : builder.in(path);
                 Arrays.asList(criteria.getValue().toString().split("_"))
-                    .forEach(v -> {
-                        if (path.getJavaType().isAssignableFrom(Long.class)) {
-                            inClause.value(Long.valueOf(v));
-                        }else if(isCaseInsensitive(criteria, path)){
-                            inClause.value(v.toLowerCase());
-                        }else{
-                            inClause.value(v);
-                        }
-                    });
+                        .forEach(v -> {
+                            if (path.getJavaType().isAssignableFrom(Long.class)) {
+                                inClause.value(Long.valueOf(v));
+                            } else if (isCaseInsensitive(criteria, path)) {
+                                inClause.value(v.toLowerCase());
+                            } else {
+                                inClause.value(v);
+                            }
+                        });
                 return inClause;
             default:
                 return null;
         }
     }
 
-    private boolean isCaseInsensitive(SearchCriteria criteria, Path path){
+    private boolean isCaseInsensitive(SearchCriteria criteria, Path path) {
         return !criteria.isCaseSensitive() && path.getJavaType() == String.class;
     }
 
